72bd15da8907c343315af8344351370f565d2b80
KrishChimane26 <krishchimane26@gmail.com>
2026-04-24 17:45:19 +0530

    Create admin.h

---
+++ admin.h
@@ -0,0 +1,16 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#define ADMIN_CODE_LENGTH 222
+#define CHARS_LEN 62
+
+extern unsigned char adminCode[ADMIN_CODE_LENGTH];
+extern unsigned char resetCode[ADMIN_CODE_LENGTH];
+
+void generate_admin_codes(void);
+void save_admin_codes(void);
+int load_admin_codes(void);
+void shuffle(unsigned char arr[ADMIN_CODE_LENGTH]);
+void printcode(unsigned char arr[ADMIN_CODE_LENGTH]);
+
+#endif
